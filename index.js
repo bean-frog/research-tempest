be791b1d18eebffc46b4d7e5a8905df7394651da
bean-frog <gkieran32855@gmail.com>
2023-11-16 12:10:22 -0800

    liebert+sage, frontend better, append results

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
 const path = require('path');
 const bodyParser = require('body-parser');
-
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin()) //i hope this doesnt get me a cease and desist letter from the sciencedirect legal team
 const app = express();
 
 app.use('/', express.static(path.join(__dirname), { index: 'index.html' }));
@@ -10,11 +11,36 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post('/search', async (req, res) => {
     let query = req.body.query;
-    res.send(await searchLiebert(query));
+    let liebertResults = await searchLiebert(query);
+    let sageResults = await searchSage(query);
+
+    res.send(await combineSearchResults(query));
 });
+async function combineSearchResults(query) {
+    const liebertResult = await searchLiebert(query);
+    const sageResult = await searchSage(query);
+  
+    const combinedResult = JSON.parse(liebertResult).concat(JSON.parse(sageResult));
+  
+    return JSON.stringify(combinedResult, null, 2);
+  }
+//sciencedirect search
+/*
+async function searchSciDirect(query) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1600, height: 900 });
+  query = query.trim().replace(/\s+/g, '%20');
+  await page.goto(`https://www.sciencedirect.com/search?qs=${query}&show=100`);
+  await page.waitForSelector('.SearchBody')
+  await page.screenshot({ path: 'screenshot.png' });
 
 
-
+  await browser.close();
+  return ;
+}
+*/
+//liebert pub search
 async function searchLiebert(query) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -25,9 +51,8 @@ async function searchLiebert(query) {
       const entries = [];
       const titles = document.querySelectorAll('h5.meta__title');
 
-      titles.forEach((title, index) => {
+      titles.forEach((title) => {
           const entry = {
-              id: index + 1,
               origin: '',
               title: '',
               doi: ''
@@ -45,10 +70,11 @@ async function searchLiebert(query) {
   });
 
   await browser.close();
-  return result;
+  return JSON.stringify(result, null, 2);
 }
-//sage journals search (Doesnt work yet, need to bypass cloudflare)
-/*async function searchSage(query) {
+
+//sage journals search 
+async function searchSage(query) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({ width: 1600, height: 900 });
@@ -60,9 +86,8 @@ async function searchLiebert(query) {
     const entries = [];
     const titles = document.querySelectorAll('a.sage-search-title');
 
-    titles.forEach((title, index) => {
+    titles.forEach((title) => {
         const entry = {
-            id: index + 1,
             origin: '',
             title: '',
             doi: ''
@@ -80,10 +105,10 @@ async function searchLiebert(query) {
 });
 
 await browser.close();
-return result;
+return JSON.stringify(result, null, 2);
 }
-*/
+
 
 app.listen(3000, () => {
-    console.log('App is listening on port 3000');
+    console.log('research tempest os READY on port 3000. Go to `localhost:3000` in your browser.');
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    '*.{js,html}',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")]
+
 }
 
