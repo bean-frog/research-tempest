d47f32514171407712acaf28393e233a98d0d57f
bean-frog <gkieran32855@gmail.com>
2023-11-16 23:43:14 -0800

    format js

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin()) //i hope this doesnt get me a cease and desist letter from the sciencedirect legal team
 const app = express();
 
-app.use('/', express.static(path.join(__dirname), { index: 'index.html' }));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/', express.static(path.join(__dirname), {
+    index: 'index.html'
+}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 app.post('/search', async (req, res) => {
     let query = req.body.query;
-   
+
     res.send(await combineSearchResults(query));
 });
 async function combineSearchResults(query) {
@@ -19,163 +23,172 @@ async function combineSearchResults(query) {
     const sageResult = await searchSage(query);
     const sciDirectResult = await searchSciDirect(query);
     const pubMedResult = await searchPubMed(query)
-  
+
     const parsedLiebertResult = JSON.parse(liebertResult);
     const parsedSageResult = JSON.parse(sageResult);
     const parsedSciDirectResult = JSON.parse(sciDirectResult);
     const parsedPubMedResult = JSON.parse(pubMedResult);
     const combinedResult = parsedLiebertResult.concat(parsedSageResult, parsedSciDirectResult, parsedPubMedResult);
-  
+
     return JSON.stringify(combinedResult, null, 2);
-  }
-  
+}
+
 //sciencedirect search
 async function searchSciDirect(query) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage()
-    await page.setViewport({ width: 1600, height: 900 });
+    await page.setViewport({
+        width: 1600,
+        height: 900
+    });
     query = query.trim().replace(/\s+/g, '%20');
     await page.goto(`https://www.sciencedirect.com/search?qs=${query}&show=100&accessTypes=openaccess`);
     await page.waitForSelector('.SearchBody');
     const result = await page.evaluate(() => {
-      const list = document.querySelectorAll('li.ResultItem');
-      var entries = []
-     
-      list.forEach((item) => {
-        const entry = {
-            origin: '',
-            title: '',
-            doi: ''
-        };
-        const titleElement = item.querySelector('h2');
-        if (titleElement) {
-          entry.title = titleElement.innerText;
-        }
-          entry.doi = "https://doi.org/" + item.getAttribute('data-doi');
-          entry.origin = 'ScienceDirect';
-        entries.push(entry);
+        const list = document.querySelectorAll('li.ResultItem');
+        var entries = []
+
+        list.forEach((item) => {
+            const entry = {
+                origin: '',
+                title: '',
+                doi: ''
+            };
+            const titleElement = item.querySelector('h2');
+            if (titleElement) {
+                entry.title = titleElement.innerText;
+            }
+            entry.doi = "https://doi.org/" + item.getAttribute('data-doi');
+            entry.origin = 'ScienceDirect';
+            entries.push(entry);
+        });
+        return entries;
     });
-      return entries;
-    });
-  
-  
+
+
     await browser.close();
     return JSON.stringify(result, null, 2);
-  
-  }
+
+}
 //liebert pub search
 async function searchLiebert(query) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1600, height: 900 });
-  query = query.trim().replace(/\s+/g, '+');
-  await page.goto(`https://www.liebertpub.com/action/doSearch?AllField=${query}&pagesize=100`);
-  const result = await page.evaluate(() => {
-      const entries = [];
-      const titles = document.querySelectorAll('h5.meta__title');
-      
-      titles.forEach((title) => {
-          const entry = {
-              origin: '',
-              title: '',
-              doi: ''
-          };
-          const titleElement = title.querySelector('a');
-          if (titleElement) {
-              entry.title = titleElement.textContent.trim();
-              entry.doi = titleElement.href;
-              entry.origin = 'LiebertPub'
-          }
-          entries.push(entry);
-      });
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 1600,
+        height: 900
+    });
+    query = query.trim().replace(/\s+/g, '+');
+    await page.goto(`https://www.liebertpub.com/action/doSearch?AllField=${query}&pagesize=100`);
+    const result = await page.evaluate(() => {
+        const entries = [];
+        const titles = document.querySelectorAll('h5.meta__title');
 
-      return entries;
-  });
+        titles.forEach((title) => {
+            const entry = {
+                origin: '',
+                title: '',
+                doi: ''
+            };
+            const titleElement = title.querySelector('a');
+            if (titleElement) {
+                entry.title = titleElement.textContent.trim();
+                entry.doi = titleElement.href;
+                entry.origin = 'LiebertPub'
+            }
+            entries.push(entry);
+        });
 
-  await browser.close();
-  return JSON.stringify(result, null, 2);
+        return entries;
+    });
+
+    await browser.close();
+    return JSON.stringify(result, null, 2);
 }
 
 //sage journals search 
 async function searchSage(query) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1600, height: 900 });
-  query = query.trim().replace(/\s+/g, '+');
-  await page.goto(`https://journals.sagepub.com/action/doSearch?AllField=${query}&startPage=0&rel=&access=user&pageSize=100`);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 1600,
+        height: 900
+    });
+    query = query.trim().replace(/\s+/g, '+');
+    await page.goto(`https://journals.sagepub.com/action/doSearch?AllField=${query}&startPage=0&rel=&access=user&pageSize=100`);
 
 
-  const result = await page.evaluate(() => {
-    const entries = [];
-    const titles = document.querySelectorAll('a.sage-search-title');
+    const result = await page.evaluate(() => {
+        const entries = [];
+        const titles = document.querySelectorAll('a.sage-search-title');
 
-    titles.forEach((title) => {
-        const entry = {
-            origin: '',
-            title: '',
-            doi: ''
-        };
-        const titleElement = title.childNodes[0]
-        if (titleElement) {
-            entry.title = titleElement.textContent.trim();
-            entry.doi = title.href;
-            entry.origin = 'Sage Journals'
-        }
-        entries.push(entry);
+        titles.forEach((title) => {
+            const entry = {
+                origin: '',
+                title: '',
+                doi: ''
+            };
+            const titleElement = title.childNodes[0]
+            if (titleElement) {
+                entry.title = titleElement.textContent.trim();
+                entry.doi = title.href;
+                entry.origin = 'Sage Journals'
+            }
+            entries.push(entry);
+        });
+
+        return entries;
     });
 
-    return entries;
-});
-
-await browser.close();
-return JSON.stringify(result, null, 2);
+    await browser.close();
+    return JSON.stringify(result, null, 2);
 }
 //nih.gov search
 async function searchPubMed(query) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setViewport({
-      width: 1600,
-      height: 900
-  });
-  query = query.trim().replace(/\s+/g, '+');
-  await page.goto(`https://pubmed.ncbi.nlm.nih.gov/?term=${query}&filter=simsearch2.ffrft&size=100`);
-  await page.waitForSelector('.search-results');
-  const result = await page.evaluate(() => {
-      const list = document.querySelectorAll('article.full-docsum');
-      var entries = []
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 1600,
+        height: 900
+    });
+    query = query.trim().replace(/\s+/g, '+');
+    await page.goto(`https://pubmed.ncbi.nlm.nih.gov/?term=${query}&filter=simsearch2.ffrft&size=100`);
+    await page.waitForSelector('.search-results');
+    const result = await page.evaluate(() => {
+        const list = document.querySelectorAll('article.full-docsum');
+        var entries = []
 
-      list.forEach((item) => {
-          const entry = {
-              origin: '',
-              title: '',
-              doi: ''
-          };
-          const titleElement = item.getElementsByClassName('docsum-title')[0];
-          if (titleElement) {
-              entry.title = titleElement.innerText;
-          }
-          var inputString = item.querySelector('.docsum-journal-citation').innerText
-          const doiRegex = /\bdoi:\s*(10\.\d+\/[^\s]+)/i;
+        list.forEach((item) => {
+            const entry = {
+                origin: '',
+                title: '',
+                doi: ''
+            };
+            const titleElement = item.getElementsByClassName('docsum-title')[0];
+            if (titleElement) {
+                entry.title = titleElement.innerText;
+            }
+            var inputString = item.querySelector('.docsum-journal-citation').innerText
+            const doiRegex = /\bdoi:\s*(10\.\d+\/[^\s]+)/i;
 
-          const match = inputString.match(doiRegex);
+            const match = inputString.match(doiRegex);
 
-          if (match && match[1]) {
-              const doi = match[1];
-              const doiUrl = "https://doi.org/" + doi;
-              inputString = doiUrl;
-              entry.doi = inputString;
-          }
+            if (match && match[1]) {
+                const doi = match[1];
+                const doiUrl = "https://doi.org/" + doi;
+                inputString = doiUrl;
+                entry.doi = inputString;
+            }
 
-          entry.origin = 'ncbi.nlm.nih.gov';
-          entries.push(entry);
-      });
-      return entries;
-  });
+            entry.origin = 'ncbi.nlm.nih.gov';
+            entries.push(entry);
+        });
+        return entries;
+    });
 
 
-  await browser.close();
-  return JSON.stringify(result, null, 2);
+    await browser.close();
+    return JSON.stringify(result, null, 2);
 
 }
 
